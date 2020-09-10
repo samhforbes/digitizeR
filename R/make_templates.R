5c4b2e737350ff1c5d6dadfdbfe5c9462a32bf46
Sam Forbes <samuel.h.forbes@gmail.com>
2020-09-10 16:28:38 +0100

    Update make_templates.R

--- R/make_templates.R
+++ R/make_templates.R
@@ -1,102 +1,38 @@
-# make_templates <- function(x){
-#   a <- unname(x)
-#   
-#   a <- lapply(a, function(y)
-#     lapply(y, function(z) unname(z)))
-#   #lapply misses the last element for now
-#   a[length(a)] <- NULL
-#   
-#   a <- do.call(rbind.data.frame, a)
-#   
-#   template <- a %>% group_by(v) %>%
-#     summarise_each(funs(mean(., na.rm = T)), x, y, z)
-#   v <- as.character(c('nz', 'ar', 'al', 'cz', 'iz'))
-#   las <- data.frame(v)
-#   las$v <- as.character(las$v)
-#   template$v <- as.character(template$v)
-#   template <- merge(las, template, by = 'v', sort = F)
-#   template <- dplyr::select(template, x, y, z, v)
-#   
-#   return(template)
-# }
-# 
-# make_full_templates <- function(data, length){
-#   v2 <- as.character(c(paste('v',seq(1,length), sep = '')))
-# 
-#   x <- lapply(data, function(y) cbind(y,v2))
-#   a <- unname(x)
-# 
-#   a <- lapply(a, function(y)
-#     lapply(y, function(z) unname(z)))
-#   #lapply misses the last element for now
-#   a[length(a)] <- NULL
-# 
-#   a <- do.call(rbind.data.frame, a)
-# 
-# 
-#   template <- a %>% group_by(v2) %>%
-#     summarise_each(funs(mean(., na.rm = T)), x, y, z)
-# 
-#   las <- data.frame(v2)
-#   las$v2 <- as.character(las$v2)
-#   template$v2 <- as.character(template$v2)
-#   template <- merge(las, template, by = 'v2', sort = F)
-#   template <- dplyr::select(template, x, y, z, v2)
-# 
-#   return(template)
-#}
-# 
-# make_gates_templates <- function(x){
-#   v2 <- as.character(c(paste('v',seq(1,37), sep = '')))
-#   
-#   x <- lapply(x, function(y) cbind(y,v2))
-#   a <- unname(x)
-#   
-#   a <- lapply(a, function(y)
-#     lapply(y, function(z) unname(z)))
-#   #lapply misses the last element for now
-#   a[length(a)] <- NULL
-#   
-#   a <- do.call(rbind.data.frame, a)
-#   
-#   
-#   template <- a %>% group_by(v2) %>%
-#     summarise_each(funs(mean(., na.rm = T)), x, y, z)
-#   
-#   las <- data.frame(v2)
-#   las$v2 <- as.character(las$v2)
-#   template$v2 <- as.character(template$v2)
-#   template <- merge(las, template, by = 'v2', sort = F)
-#   template <- dplyr::select(template, x, y, z, v2)
-#   
-#   return(template)
-# }
+#' create partial preliminary templates from data
+#'
+#' This is used as a first step before creating the final template.
+#' The idea is just to check we are on the right track.
+#'
+#' @param template_index output of find_greatest_alignment
+#' @param data raw data output of select_caps_by_npoints
+#' @return one sample cap for each cap size
+#' @export
 
-make_templates <- function(template_index, data){
-  
+make_partial_templates <- function(template_index, data){
+  #used to be make_templates
   new_templates <- list()
   for(i in 1:length(template_index)){
     ind <- as.numeric(template_index[[i]])
-    
+
     new_templates[[i]] <- data[[i]][[ind]]
   }
-  
+
   new_templates <- lapply(new_templates, function(x) x[,2:4])
-  
+
   return(new_templates)
 }
 
 make_full_templates <- function(data, template_index, distances, length){
-  
+
 #  data <- lapply(data, function(x)
 #    lapply(x, function(y) y[,2:4]))
-  
+
   data_list <- list()
   for(i in 1:length(template_index)){
     nam <- names(template_index[[i]])
     caps <- list()
     caps <- distances[[i]][[nam]]
-    
+
     mylist <- list()
     for(j in 1:length(caps)){
       k <- caps[[j]]
@@ -104,13 +40,13 @@ make_full_templates <- function(data, template_index, distances, length){
     }
     data_list[[i]] <- mylist
   }
-  
+
   #then make the means
   data_list2 <- data_list
   for(i in 1:length(data_list2)){
     v2 <- as.character(c(paste('v',seq(1,length), sep = '')))
        x <- lapply(data_list[[i]], function(y) cbind(y,v2))
-       
+
        a <- do.call(rbind.data.frame, x)
 
 
@@ -125,7 +61,7 @@ make_full_templates <- function(data, template_index, distances, length){
          template$v2 <- as.character(template$v2)
          template <- merge(las, template, by = 'v2', sort = F)
          template <- dplyr::select(template, x, y, z)
-         
+
          data_list2[[i]] <- template
   }
   return(data_list2)
